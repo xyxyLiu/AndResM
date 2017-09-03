0a17b2c75d44fc4dd904e9d0e7261d2a2596c747
xyxyLiu <tonyreginald@gmail.com>
2017-09-03 22:05:01 +0800

    init project

---
+++ andresm/src/main/java/com/reginald/andresm/Test.java
@@ -0,0 +1,4 @@
+package com.reginald.andresm;
+
+public class Test {
+}
---
+++ app/src/main/java/com/reginald/andresm/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.reginald.andresm.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.reginald.andresm.R.layout.activity_main);
+    }
+}
